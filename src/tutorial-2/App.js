5d1557513dd9c58865e7d7fa3b86638af2a6aa38
Shiroyam-token <semenmaslina@gmail.com>
2022-02-02 22:43:46 +0300

    Сделал  урок № 2

---
+++ src/tutorial-2/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Form = () => {
+  let email;
+  let password;
+  
+    const changeCheck = (e) => {
+        if(e.target.name === 'email'){
+            email = e.target.value
+            console.log(email)
+        } else if (e.target.name === 'password') {
+            password = e.target.value
+            console.log(password)
+        }
+    }
+
+    const handleSubmit = (e) => {
+        
+        if (password === undefined || email === undefined){
+            alert('Заполните поля!')
+        } else if (!email.trim() || !password.trim()) {
+			alert('Вы заполнили поле пробелами!');
+        } else if (email && password){
+			console.log({ email, password });
+			email = '';
+			password = '';
+			e.target.reset();
+		} 
+		e.preventDefault();
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input onChange={changeCheck}  type={'email'} placeholder={'E-Mail'}  name="email"></input>
+                <input onChange={changeCheck} type={'password' } placeholder={'Пароль'}  name="password"></input>
+                <button  type={'submit'}><div>Войти</div></button>
+            </form>
+        </>
+    );
+}
+
+const App = () =>{
+    return (
+    <Form />
+    );
+} 
+
+export default App;
